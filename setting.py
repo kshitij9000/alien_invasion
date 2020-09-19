d8c819bff131cc83135cab4aed987bc2bf3e4459
kshitij9000 <bhandarikshitij9000@gmail.com>
2020-09-19 10:09:25 +0545

    added setting to store all the settings in a different class and the main file clear

---
+++ setting.py
@@ -0,0 +1,9 @@
+class Setting:
+    """ A class to store all the settings for Alien Invasion. """
+
+    def __init__(self):
+        """ Initialize the game setting. """
+        # screen setting
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
