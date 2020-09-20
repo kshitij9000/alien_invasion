f197692f88444fbfba67e950775bfe5b01550774
kshitij9000 <bhandarikshitij9000@gmail.com>
2020-09-20 17:46:41 +0545

    added some comments in ship.py

--- ship.py
+++ ship.py
@@ -8,9 +8,13 @@ class Ship:
         """ Initialize the ship and set its starting position. """
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        # needed only when the color of pygame windows is different than that of the image
+        # since I am using grey theme I will not use self.bg_cp;pr and self.image.set_colorkey()
+        # self.bg_color = (230, 230, 230)
 
         # load the ship image and get its rect.
         self.image = pygame.image.load('Images/ship.bmp')
+        # self.image.set_colorkey(self.bg_color)
         self.rect = self.image.get_rect()
 
         # start new ship at the bottom center of the screen
