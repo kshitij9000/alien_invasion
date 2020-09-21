c25e8fdb05deee258e211b50721c9ddcb8205b05
kshitij9000 <bhandarikshitij9000@gmail.com>
2020-09-21 13:30:14 +0545

    added bullet settings

--- setting.py
+++ setting.py
@@ -9,3 +9,8 @@ class Setting:
         self.bg_color = (230, 230, 230)
         # ship setting
         self.ship_speed = 1.5
+        # bullet setting
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
