630e5612e72a031249ff5517044abd7e4701925d
kshitij9000 <bhandarikshitij9000@gmail.com>
2020-09-23 12:03:14 +0545

    set self.game_active to True so that the game will not last forever and we can False this value to end the game

--- game_stats.py
+++ game_stats.py
@@ -6,6 +6,9 @@ class GameStats:
         self.setting = ai_games.setting
         self.reset_stats()
 
+        # start Alien Invasion in active state.
+        self.game_active = True
+
     def reset_stats(self):
         """ Initialize statistics that can change during the game. """
         self.ship_left = self.setting.ship_limit
