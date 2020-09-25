283598d8b4a11935fcf924e8ebb34b091e701573
kshitij9000 <bhandarikshitij9000@gmail.com>
2020-09-25 08:56:07 +0545

    increasing bullet points after each level

--- setting.py
+++ setting.py
@@ -21,14 +21,17 @@ class Settings:
         self.fleet_drop_speed = 10
 
         # how quickly the game speed's up
-        self.speedup_scale = 1.1
+        self.speedup_scale = 1.5
+
+        # how quickly does point value increases
+        self.score_scale = 1.5
 
         self.initialize_dynamic_setting()
 
     def initialize_dynamic_setting(self):
         """Initialize settings that change throughout the game."""
 
-        self.ship_speed = 1.5
+        self.ship_speed = 1.1
         self.bullet_speed = 3.0
         self.alien_speed = 1.0
         # fleet_direction of 1 represents right; -1 represents left.
@@ -38,7 +41,9 @@ class Settings:
         self.alien_points = 50
 
     def increase_speed(self):
-        """ Increase speed settings. """
+        """ Increase speed settings and alien points."""
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
